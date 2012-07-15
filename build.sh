fb9dccf3378e9d63b6ab446b6da3571961def514
Erland Isaksson <erland@isaksson.info>
2012-07-15 13:53:40 +0200

    Corrected build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-rm -rf /tmp/IckStream
-mkdir /tmp/IckStream
-cp applet/IckStreamApplet.lua /tmp/IckStream/.
-cp applet/IckStreamMeta.lua /tmp/IckStream/.
-cp applet/strings.txt /tmp/IckStream/.
-cp daemon/ickSocketDaemon /tmp/IckStream/.
-DIR=`pwd`
-cd /tmp
-zip -r $DIR/IckStreamSqueezebox-0.1.`date +%s`.zip IckStream
-cd $DIR
+zip -j IckStreamSqueezebox-0.1.`date +%s`.zip daemon/ickSocketDaemon applet/IckStreamApplet.lua applet/IckStreamMeta.lua applet/strings.txt
