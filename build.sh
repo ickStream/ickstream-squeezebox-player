1b5fd89b0aa8622caebddb7af5ea5caf626d750f
Erland Isaksson <erland@isaksson.info>
2012-07-17 12:01:24 +0200

    Updated so it builds applet with local playlist

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -j IckStreamSqueezebox-0.1.`date +%s`.zip daemon/ickSocketDaemon applet/IckStreamApplet.lua applet/IckStreamMeta.lua applet/strings.txt
+zip -j IckStreamSqueezebox-0.1.`date +%s`.zip daemon/ickSocketDaemon appletlocalplaylist/IckStreamApplet.lua appletlocalplaylist/IckStreamMeta.lua appletlocalplaylist/strings.txt
