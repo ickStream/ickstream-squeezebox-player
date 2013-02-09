63195aa2620281600b0f5060be54f5ddc6d1ffcd
Erland Isaksson <erland@isaksson.info>
2013-02-09 07:39:30 +0100

    Adjusted so build number if generated properly and a working repository file is generated

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-zip -j IckStreamSqueezebox-0.1.`date +%s`.zip daemon/ickSocketDaemon appletlocalplaylist/IckStreamApplet.lua appletlocalplaylist/IckStreamMeta.lua appletlocalplaylist/strings.txt appletlocalplaylist/LICENSE.txt
+BUILDVERSION="0.1.`date +%s`"
+rm -rf target
+mkdir target
+zip -j target/IckStreamSqueezebox-$BUILDVERSION.zip daemon/ickSocketDaemon appletlocalplaylist/IckStreamApplet.lua appletlocalplaylist/IckStreamMeta.lua appletlocalplaylist/strings.txt appletlocalplaylist/LICENSE.txt
+sed "s/BUILDVERSION/$BUILDVERSION/" repository-squeezebox.xml > target/repository-squeezebox.xml
